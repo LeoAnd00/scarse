f498a17ebb562f66e6a2fae540d505321a14ae6a
Leo Andrekson <leo.andrekson@gmail.com>
2026-05-27 15:27:28 +0200

    Update __init__.py

--- scarse/__init__.py
+++ scarse/__init__.py
@@ -62,7 +62,6 @@ def train(data_path,
     global scarse_env
 
     try:
-        #df = pd.read_csv(data_path)
         if data_path.endswith((".xlsx", ".xls")):
             df = pd.read_excel(data_path)
         df = pd.read_csv(data_path, sep=None, engine="python")
